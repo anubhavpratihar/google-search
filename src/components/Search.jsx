ad7a8beafe6e1b9d27ebe4caf94b43ed91c6f6d6
Anubhav Pratihar <117383083+anubhavpratihar@users.noreply.github.com>
2023-05-19 08:15:21 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,25 +5,32 @@ import { useStateContext } from '../contexts/StateContextProvider';
 import { Links } from './Links';
 
 export const Search = () => {
-  const { setSearchTerm } = useStateContext();
+  const { setSearchTerm, searchTerm } = useStateContext();
   const [text, setText] = useState('Elon Musk');
   const [debouncedValue] = useDebounce(text, 300);
 
   useEffect(() => {
-    if (debouncedValue) setSearchTerm(debouncedValue);
-  }, [debouncedValue]);
+    if (debouncedValue && debouncedValue !== searchTerm) {
+      setSearchTerm(debouncedValue);
+    }
+  }, [debouncedValue, setSearchTerm, searchTerm]);
 
   return (
     <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
       <input
+        key="search-input"
         value={text}
         type="text"
-        className="sm:w-96 w-80 h-10 dark:bg-gray-200  border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+        className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
         placeholder="🔎 Search Google or type URL"
         onChange={(e) => setText(e.target.value)}
       />
       {text !== '' && (
-        <button type="button" className="absolute top-1.5 right-4 text-2xl text-gray-500 " onClick={() => setText('')}>
+        <button
+          type="button"
+          className="absolute top-1.5 right-4 text-2xl text-gray-500"
+          onClick={() => setText('')}
+        >
           x
         </button>
       )}
