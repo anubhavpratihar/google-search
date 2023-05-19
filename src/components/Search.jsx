bc0dac1073010904c2bccaee6031cc924bbdd4cc
Anubhav Pratihar <117383083+anubhavpratihar@users.noreply.github.com>
2023-05-19 08:41:31 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,47 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { useDebounce } from 'use-debounce';
+
 import { useStateContext } from '../contexts/StateContextProvider';
-import { Links } from './Links';
+import { getSearchResults } from '../api/api';
 
 export const Search = () => {
-  const { setSearchTerm } = useStateContext();
-  const [text, setText] = useState('Elon Musk');
-  const [debouncedValue] = useDebounce(text, 300);
+  const { setSearchResults } = useStateContext();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if (debouncedValue) {
-      setSearchTerm(debouncedValue);
-    }
-  }, [debouncedValue, setSearchTerm]);
+    const fetchSearchResults = async () => {
+      if (debouncedSearchTerm.trim() !== '') {
+        try {
+          const results = await getSearchResults(debouncedSearchTerm);
+          setSearchResults(results);
+        } catch (error) {
+          console.log('Error fetching search results:', error);
+        }
+      } else {
+        setSearchResults([]);
+      }
+    };
 
-  const handleInputChange = (e) => {
-    setText(e.target.value);
-  };
-
-  const clearSearch = () => {
-    setText('');
-    setSearchTerm('');
-  };
+    fetchSearchResults();
+  }, [debouncedSearchTerm, setSearchResults]);
 
   return (
     <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
       <input
-        value={text}
         type="text"
         className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
         placeholder="🔎 Search Google or type URL"
-        onChange={handleInputChange}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      {text !== '' && (
-        <button
-          type="button"
-          className="absolute top-1.5 right-4 text-2xl text-gray-500"
-          onClick={clearSearch}
-        >
-          x
-        </button>
-      )}
-      <Links />
     </div>
   );
 };
